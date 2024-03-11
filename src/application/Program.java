5faf8844f51601d45ee79f5f05677d6b275b39bf
Luiz <luiz04faria@gmail.com>
2024-03-11 17:52:54 -0300

    teste

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Board board = new Board (8,8);
+		Board board = new Board (8,9);
+		
+		System.out.println();
+		System.out.println();
 		
 		
 	}
