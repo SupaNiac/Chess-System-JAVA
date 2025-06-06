c37b6d01b60aa67c3bd590839e460deb1cb30716
Luiz Henrique <148809830+SupaNiac@users.noreply.github.com>
2025-06-06 02:57:24 -0300

    Update Piece.java

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,14 +1,12 @@
 package boardgame;
 
 public abstract class Piece {
-
 	protected Position position;
 	private Board board;
 
 	public Piece(Board board) {
-		super();
 		this.board = board;
-		position = null;
+		this.position = null;
 	}
 
 	protected Board getBoard() {
@@ -21,15 +19,15 @@ public abstract class Piece {
 		return possibleMoves()[position.getRow()][position.getColumn()];
 	}
 
-	public boolean isThereAnyPossibleMoves() {
+	public boolean isThereAnyPossibleMove() {
 		boolean[][] mat = possibleMoves();
-		for (int i=0; i<mat.length; i++) {
-			for (int j=0; j<mat[i].length; j++) {
-				if(mat[i][j]) {
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if (mat[i][j]) {
 					return true;
 				}
 			}
 		}
-		return false; 
+		return false;
 	}
 }
